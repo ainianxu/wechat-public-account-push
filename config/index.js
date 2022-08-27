9691b5bd3ab052cacf29586aec87d7fbe93f0d12
ainianxu <53597509+ainianxu@users.noreply.github.com>
2022-08-27 02:54:56 +0000

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx9d6ec1e3e38f8cc2",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "dd8e48a863abb48d6ca71e3bf928a0ff",
     // 模板消息id
-    templateId: "",
+    templateId: "oEKedotic2cocFTp2oeYLckZTHDqovEkjcBFEjPm390",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [oEKedotic2cocFTp2oeYLckZTHDqovEkjcBFEjPm390],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "河北",
     // 所在城市
-    city: "惠州",
+    city: "邢台",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
