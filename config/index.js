5fdd4959cd90491e83ac22989bf15b0b1cd19e3e
ainianxu <53597509+ainianxu@users.noreply.github.com>
2022-08-28 14:53:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,13 +16,11 @@ export const config = {
     city: "邢台",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "老婆", "year": "1999", "date": "10-20", "type": 'new'},
+      {"name": "家公", "year": "1999", "date": "09-08", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
+    loveDate: "2018-06-18"
     }
 
 // {{date.DATA}}  
@@ -30,8 +28,7 @@ export const config = {
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
+// 今天是我们恋爱的第{{love_day.DATA}}
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
